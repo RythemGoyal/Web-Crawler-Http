423163a2af393e89739e8abe4d254feb33206c0e
Rythem <imrythem16@gmail.com>
2024-01-24 21:20:43 +0530

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,7 +1,24 @@
 const { JSDOM } = require("jsdom");
 
-async function crawlPage(currentURL) {
+async function crawlPage(baseURL, currentURL, pages) {
+  
+  const baseURLObj = new URL(baseURL)
+  const currentURLObj = new URL (currentURL)
+  if(baseURLObj.hostname !== currentURLObj.hostname){
+    return pages
+  }
+
+  const normalizedCurrentURL = normaliseURL(currentURL)
+  if(pages[normalizedCurrentURL] > 0){
+    pages[normalizedCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizedCurrentURL] = 1;
+
   console.log(`Actively crawling : ${currentURL}`);
+
+
   try {
     const resp = await fetch(currentURL);
 
@@ -9,7 +26,7 @@ async function crawlPage(currentURL) {
       console.log(
         `error in fetch with status code ${resp.status} on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
     const contentType = resp.headers.get("content-type");
@@ -17,13 +34,20 @@ async function crawlPage(currentURL) {
       console.log(
         `non html response, content type: ${contentType}, on page: ${currentURL}`
       );
-      return;
+      return pages;
     }
 
-    console.log(await resp.text());
+    const htmlBody = await resp.text();
+
+    const nextURLs = getUrlsFromHtml(htmlBody,baseURL)
+
+    for(const nextURL of nextURLs){
+      pages = await crawlPage(baseURL, nextURL, pages)
+    }
   } catch (err) {
     console.log(`error in fetch: ${err.message} on page ${currentURL}`);
   }
+  return pages
 }
 
 function getUrlsFromHtml(htmlBody, baseUrl) {
